ce2ae1cbdad2d5b3f2545de033facc9ac3c23fa7
PanfilovDenis <panf.denis@gmail.com>
2014-01-11 08:05:42 +0000

    add todo list

--- app/assets/javascripts/welcome.js.jsx
+++ app/assets/javascripts/welcome.js.jsx
@@ -3,21 +3,34 @@
 
 var PerfectTodoApp = React.createClass({
   getInitialState: function() {
-    return { items: ["test", "hyest"] };
+    return { items: ["test", "hyest"], text: "" };
+  },
+
+  createItem: function(e) {
+    this.state.items.push(this.state.text);
+    this.setState({items: this.state.items});
+  },
+
+  changeText: function(e) {
+    this.setState({text: e.target.value});
   },
 
   render: function() {
-    console.log(this.state.items);
     return (
-      <ul>
-      {
-        _.map(this.state.items, function(item) {
-          return <li>
-            {item}
-          </li>
-        })
-      }
-      </ul>);
+      <div>
+        <input onChange={this.changeText}/>
+        <button onClick={this.createItem}>add</button>
+        <ul>
+        {
+          _.map(this.state.items, function(item) {
+            return <li>
+              {item}
+            </li>
+          })
+        }
+        </ul>
+      </div>
+    );
   }
 
 

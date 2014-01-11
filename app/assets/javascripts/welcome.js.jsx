8cf138f21721ef1d391d9d2fde37608f255c0b98
Andrey Subbota <subbota@undev.ru>
2014-01-11 13:42:32 +0400

    New feature: remove Item

--- app/assets/javascripts/welcome.js.jsx
+++ app/assets/javascripts/welcome.js.jsx
@@ -1,34 +1,55 @@
 /** @jsx React.DOM */
-
+var Item = React.createClass({
+  render: function() {
+    return (
+      <li>
+        <span className="text">{this.props.value}</span>
+        <button onClick={this.props.removeItem} value={this.props.id}>Remove</button>
+      </li>
+    );
+  }
+});
 
 var PerfectTodoApp = React.createClass({
   getInitialState: function() {
-    return { items: ["test", "hyest"], text: "" };
+    return { items: [], text: "", nextId: 0};
+  },
+
+  removeItem: function(item) {
+    var newState = {
+      items: _.without(this.state.items, item)
+    }
+
+    this.setState(newState);
   },
 
   createItem: function(e) {
-    this.state.items.push(this.state.text);
-    this.setState({items: this.state.items});
+    var newState = {
+      items: this.state.items.push({id: this.state.nextId, text: this.state.text}),
+      nextId: this.state.nextId + 1,
+      text: ""
+    }
+
+    this.setState(newState);
   },
 
   changeText: function(e) {
-    this.setState({text: e.target.value});
+    var newState = {
+      text: e.target.value
+    };
+    this.setState(newState);
   },
 
   render: function() {
     return (
       <div>
-        <input onChange={this.changeText}/>
+        <input onChange={this.changeText} value={this.state.text} />
         <button onClick={this.createItem}>add</button>
-        <ul>
-        {
+        <ul>{
           _.map(this.state.items, function(item) {
-            return <li>
-              {item}
-            </li>
-          })
-        }
-        </ul>
+             return(<Item id={item.id} value={item.text} removeItem={this.removeItem.bind(this, item)} />)
+          }, this)
+        }</ul>
       </div>
     );
   }

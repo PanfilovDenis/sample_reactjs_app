ae5709f22540f02b56c247895425f0fd66fe42f6
PanfilovDenis <panf.denis@gmail.com>
2014-01-11 11:12:18 +0000

    ad feature% edit todo list

--- app/assets/javascripts/welcome.js.jsx
+++ app/assets/javascripts/welcome.js.jsx
@@ -1,12 +1,23 @@
 /** @jsx React.DOM */
 var Item = React.createClass({
   render: function() {
-    return (
-      <li>
-        <span className="text">{this.props.value}</span>
-        <button onClick={this.props.removeItem} value={this.props.id}>Remove</button>
-      </li>
-    );
+    switch(this.props.item.state) {
+      case "uncomplited":
+        return (
+          <li>
+            <span onClick={this.props.editItem} className="text">{this.props.item.id}: {this.props.item.text}</span>
+            <button onClick={this.props.removeItem} value={this.props.item.id}>Remove</button>
+          </li>
+        );
+      case "editable":
+        return (
+          <li>
+            <input onChange={this.props.updateDraft} value={this.props.item.draftText} />
+            <button onClick={this.props.saveItem} value={this.props.item.id}>Save</button>
+          </li>
+        );
+    }
+
   }
 });
 
@@ -23,9 +34,55 @@ var PerfectTodoApp = React.createClass({
     this.setState(newState);
   },
 
-  createItem: function(e) {
+  editItem: function(item) {
+    var key = _.findKey(this.state.items, item);
+    item.state = "editable";
+    this.state.items[key] = item;
+
     var newState = {
-      items: this.state.items.push({id: this.state.nextId, text: this.state.text}),
+      items: this.state.items
+    }
+
+    this.setState(newState);
+  },
+
+  updateDraft: function(item, e) {
+    var key = _.findKey(this.state.items, item);
+    item.draftText = e.target.value;
+
+    this.state.items[key] = item;
+
+    var newState = {
+      items: this.state.items
+    }
+
+    this.setState(newState);
+  },
+
+  saveItem: function(item) {
+    var key = _.findKey(this.state.items, item);
+    item.text = item.draftText;
+    item.state = "uncomplited";
+
+    this.state.items[key] = item;
+
+    var newState = {
+      items: this.state.items
+    }
+
+    this.setState(newState);
+  },
+
+  createItem: function() {
+    this.state.items.push({
+      id: this.state.nextId,
+      text: this.state.text,
+      state: "uncomplited",
+      draftText: this.state.text
+    })
+
+    var newState = {
+      items: this.state.items,
       nextId: this.state.nextId + 1,
       text: ""
     }
@@ -47,7 +104,14 @@ var PerfectTodoApp = React.createClass({
         <button onClick={this.createItem}>add</button>
         <ul>{
           _.map(this.state.items, function(item) {
-             return(<Item id={item.id} value={item.text} removeItem={this.removeItem.bind(this, item)} />)
+             return(<Item
+                      item={item}
+                      removeItem={this.removeItem.bind(this, item)}
+                      editItem={this.editItem.bind(this, item)}
+                      updateDraft={this.updateDraft.bind(this, item)}
+                      saveItem={this.saveItem.bind(this, item)}/>)
+
+
           }, this)
         }</ul>
       </div>
